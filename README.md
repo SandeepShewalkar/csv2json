18cf43c18abddbab06e31ad81bbd83edae1ef79d
Sandeep Shewalkar <shewalkar.sandeep@gmail.com>
2019-11-14 17:48:16 +0530

    New functionality introduced

--- README.md
+++ README.md
@@ -1,14 +1,61 @@
 # csv2json
 
-This small package will be helpful to covert csv file data to json array.
 
-# Sample
+Simplified way to read csv files and operate over data.
+This package is helpful to :
+	1. covert csv file data to JSON array.
+	2. Get data from desired column
+	3. Get data from desired row
+	4. Get data by excluding columns
+	5. Column wise data sorting
 
-func main() {
 
-	jsonData := csv2json.ConverToJSONArray("/home/Records.csv")
+# Installation
 
-	for _, val := range jsonData {
-		fmt.Println(string(val))
-	}
-}
+go get github.com/SandeepShewalkar/csv2json
+
+
+# Sample Usage
+
+
+	ConverToJSONArray: 
+	-----------------
+	
+	csv2json.ConverToJSONArray("/home/Records.csv")
+
+	This function gets all data in the form of JSON array
+
+
+
+	GetRowToJSON:
+	-------------
+
+	csv2json.GetRowToJSON("/home/Records.csv", 14)
+
+	This function gets data from row number 14
+
+
+
+	GetColumnToJSON:
+	----------------
+	csv2json.GetColumnToJSON("/home/Records.csv", 3)
+
+	This function gets data from column number 3
+
+
+
+	GetJSONByExcludingColumns:
+	-------------------------
+
+	csv2json.GetJSONByExcludingColumns("/home/Records.csv", 1, 14)
+
+	This function gets data by excluding column 1 and 14
+
+
+	
+	GetSortedJSON:
+	-------------
+	
+	csv2json.GetSortedJSON("/home/Records.csv", "Country", csv2json.DESCENDING, csv2json.STRING)
+	
+	This function first sorts and then returns the sorted data. 
--- csv2json/csv2json.go
+++ csv2json/csv2json.go
@@ -6,6 +6,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+)
+
+// const (
+// 	INTEGER = "INTEGER"
+// 	STRING  = "STRING"
+// )
+
+type ColumnType int
+type SortType int
+
+const (
+	INTEGER ColumnType = iota
+	STRING  ColumnType = iota
+)
+const (
+	ASCENDING  SortType = iota
+	DESCENDING SortType = iota
 )
 
 func readCSV(path string) [][]string {
@@ -44,4 +63,156 @@ func ConverToJSONArray(csvPath string) [][]byte {
 
 	}
 	return byteArray
+}
+
+// GetRowToJSON This func will return the data from the desired row.
+// You can also get all the headers, by sending 1 as row number
+func GetRowToJSON(csvPath string, rowNumber int) [][]byte {
+	lines := readCSV(csvPath)
+	if rowNumber > 0 && rowNumber < len(lines) {
+		byteArray := [][]byte{}
+		jsonMap := map[string]string{}
+		for j := 0; j < len(lines[0]); j++ {
+			jsonMap[lines[0][j]] = lines[rowNumber-1][j]
+		}
+		json, err := json.Marshal(jsonMap)
+		if err != nil {
+			log.Println("Marshal error : ", err)
+			return nil
+		}
+		byteArray = append(byteArray, json)
+		return byteArray
+	}
+	log.Println("Row number should not be greater than total rows and less than 1")
+	return nil
+
+}
+
+func GetColumnToJSON(csvPath string, columnNumber int) [][]byte {
+	lines := readCSV(csvPath)
+	totalNumOfColumns := 0
+	if len(lines) > 0 {
+		totalNumOfColumns = len(lines[0])
+	}
+
+	if columnNumber > 0 && columnNumber <= totalNumOfColumns {
+
+		byteArray := [][]byte{}
+		c := columnNumber - 1
+		for i := 0; i < len(lines); i++ {
+			if i == 0 {
+				continue
+			}
+			jsonMap := map[string]string{}
+
+			jsonMap[lines[0][c]] = lines[i][c]
+
+			json, err := json.Marshal(jsonMap)
+			if err != nil {
+				log.Println("Marshal error : ", err)
+				return nil
+			}
+			byteArray = append(byteArray, json)
+
+		}
+		return byteArray
+	}
+	log.Println("Column number should be less than total number of columns and greater than 0")
+	return nil
+}
+
+func GetJSONByExcludingColumns(csvPath string, columnNumbers ...int) [][]byte {
+	lines := readCSV(csvPath)
+	byteArray := [][]byte{}
+	for i := 0; i < len(lines); i++ {
+		if i == 0 {
+			continue
+		}
+		jsonMap := map[string]string{}
+		for j := 0; j < len(lines[0]); j++ {
+			if !contains(columnNumbers, (j + 1)) {
+				jsonMap[lines[0][j]] = lines[i][j]
+			}
+		}
+		json, err := json.Marshal(jsonMap)
+		if err != nil {
+			log.Println("Marshal error : ", err)
+			return nil
+		}
+		byteArray = append(byteArray, json)
+
+	}
+	return byteArray
+}
+
+func GetSortedJSON(csvPath string, sortBy string, sortType SortType, colType ColumnType) [][]byte {
+
+	lines := readCSV(csvPath)
+	byteArray := [][]byte{}
+	arrOfMaps := []map[string]string{}
+
+	for i := 0; i < len(lines); i++ {
+		if i == 0 {
+			continue
+		}
+		jsonMap := map[string]string{}
+		for j := 0; j < len(lines[0]); j++ {
+			jsonMap[lines[0][j]] = lines[i][j]
+		}
+		arrOfMaps = append(arrOfMaps, jsonMap)
+
+	}
+
+	sortColumns(arrOfMaps, sortBy, sortType, colType)
+
+	for _, val := range arrOfMaps {
+		json, err := json.Marshal(val)
+		if err != nil {
+			log.Println("Marshal error : ", err)
+			return nil
+		}
+		byteArray = append(byteArray, json)
+	}
+
+	return byteArray
+
+}
+
+func sortColumns(arrOfMaps []map[string]string, sortBy string, sortType SortType, colType ColumnType) {
+
+	switch colType {
+	case INTEGER:
+		sort.Slice(arrOfMaps[:], func(i, j int) bool {
+			ival, _ := strconv.Atoi(arrOfMaps[i][sortBy])
+			jval, _ := strconv.Atoi(arrOfMaps[j][sortBy])
+			if sortType == ASCENDING {
+				return ival < jval
+
+			} else {
+				return ival > jval
+
+			}
+		})
+	case STRING:
+		sort.Slice(arrOfMaps[:], func(i, j int) bool {
+			ival := arrOfMaps[i][sortBy]
+			jval := arrOfMaps[j][sortBy]
+			if sortType == ASCENDING {
+				return ival < jval
+
+			} else {
+				return ival > jval
+
+			}
+		})
+	}
+}
+
+func contains(arr []int, num int) bool {
+	for _, index := range arr {
+		if index == num {
+			return true
+		}
+	}
+	return false
 }
